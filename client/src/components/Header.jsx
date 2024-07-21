a5c5f249047872d8c8772ae81be1e673c13a83eb
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-07-21 17:36:10 +0530

    create Header component and change favicon

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <header className="bg-slate-200 shadow-md">
+            <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+                <Link to="/">
+                    <h1 className="text-sm md:text-3xl font-bold flex flex-wrap">
+                        <span className="text-slate-500">Citi</span>
+                        <span className="text-slate-700">Luxe</span>
+                    </h1>
+                </Link>
+
+                <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        className="bg-transparent focus:outline-none w-24 sm:w-60"
+                    />
+                    <FaSearch className="text-slate-500" />
+                </form>
+
+                <ul className="flex gap-5 cursor-pointer">
+                    <Link to="/">
+                        <li className="hidden sm:inline sm:text-lg text-slate-700 hover:text-red-600">
+                            Home
+                        </li>
+                    </Link>
+                    <Link to="about">
+                        <li className="hidden sm:inline sm:text-lg text-slate-700 hover:text-red-600">
+                            About
+                        </li>
+                    </Link>
+                    <Link to="sign-in">
+                        <li className="sm:text-lg text-slate-700 hover:text-red-600">
+                            Sign In
+                        </li>
+                    </Link>
+                </ul>
+            </div>
+        </header>
+    );
+}
