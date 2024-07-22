7e70ad358a672f8f6aa3e5b1e629de668ab47553
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-07-22 17:07:26 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, password, email } = req.body;
@@ -10,6 +12,32 @@ export const signup = async (req, res, next) => {
         res.status(201).json("User created successfully");
     } catch (error) {
         // This error handling middleware is defined in index.js file
+        next(error);
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            next(errorHandler(404, "User not found"));
+        }
+        const validPassword = bcryptjs.compareSync(
+            password,
+            validUser.password
+        );
+        if (!validPassword) {
+            next(errorHandler(400, "Invalid password"));
+        }
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const {password: hashedPassword, ...user} = validUser._doc;
+
+        res.cookie("access_token", token, { httpOnly: true })
+            .status(200)
+            .json(user);
+    } catch (error) {
         next(error);
     }
 };
