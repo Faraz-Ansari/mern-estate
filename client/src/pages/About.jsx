a8740a714946231b9bda4f5495f63526f69b305b
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-07-29 01:47:50 +0530

    complete About page UI

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,35 @@
-import React from "react";
-
 export default function About() {
-    return <div>About</div>;
+    return (
+        <div className="py-10  px-4 max-w-6xl mx-auto">
+            <h1 className="text-4xl font-bold mb-4 text-slate-800">
+                About CrestaView
+            </h1>
+            <p className="mb-4 text-slate-700 ">
+                CrestView is a leading real estate website that revolutionizes
+                the home-buying experience by offering an intuitive interface
+                and advanced search tools. Users can easily explore a wide range
+                of properties, from city apartments to luxury estates, with
+                high-quality images and virtual tours that provide a
+                comprehensive view of each listing. CrestView ensures you have
+                all the necessary information to find your ideal home.
+            </p>
+            <p className="mb-4 text-slate-700 ">
+                CrestView stands out with its personalized service and expert
+                guidance. Our team of experienced real estate professionals is
+                committed to offering tailored advice for buyers, sellers, and
+                investors alike. With access to real-time market data,
+                neighborhood insights, and mortgage calculators, users can
+                confidently navigate the complexities of real estate
+                transactions.
+            </p>
+            <p className="mb-4 text-slate-700 ">
+                At CrestView, we prioritize customer satisfaction and aim to
+                make the home-buying process enjoyable and stress-free. From the
+                initial search to closing the deal, CrestView is your trusted
+                partner in real estate. Join the many satisfied clients who have
+                found their dream homes with CrestView, and let us help you
+                unlock the door to your future.
+            </p>
+        </div>
+    );
 }
