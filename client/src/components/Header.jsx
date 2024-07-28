fe3bf9b256b735d32ed0e2765ce2cba96d0a4491
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-07-28 15:15:57 +0530

    complete Header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,43 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Header() {
     const { currentUser } = useSelector((state) => state.user);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const navigateTo = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Create a URLSearchParams object from the current window's URL search parameters
+        const urlParams = new URLSearchParams(window.location.search);
+        
+        // Set the "searchTerm" parameter to the value of the searchTerm variable
+        urlParams.set("searchTerm", searchTerm);
+        
+        // Convert the URLSearchParams object to a query string
+        const searchQuery = urlParams.toString();
+        
+        // Navigate to the search page with the updated query string
+        navigateTo(`/search?${searchQuery}`);
+    };
+
+    useEffect(() => {
+        // Create a URLSearchParams object from the current location's search parameters
+        const urlParams = new URLSearchParams(location.search);
+        
+        // Retrieve the value of the "searchTerm" parameter from the URL
+        const searchTerm = urlParams.get("searchTerm");
+        
+        // Update the state with the retrieved search term, or set it to an empty string if not found
+        setSearchTerm(searchTerm || "");
+
+    // Re-run the effect whenever the search parameters in the URL change
+    }, [location.search]);
+
+
     return (
         <header className=" shadow-md">
             <div className="flex justify-between items-center max-w-6xl mx-auto p-2">
@@ -14,13 +48,20 @@ export default function Header() {
                     </h1>
                 </Link>
 
-                <form className="bg-slate-200 p-3 rounded-lg flex items-center">
+                <form
+                    onSubmit={handleSubmit}
+                    className="bg-slate-200 p-3 rounded-lg flex items-center"
+                >
                     <input
                         type="text"
                         placeholder="Search..."
                         className="bg-transparent focus:outline-none w-24 sm:w-60"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
                     />
-                    <FaSearch className="text-slate-500" />
+                    <button>
+                        <FaSearch className="text-slate-500" />
+                    </button>
                 </form>
 
                 <ul className="flex gap-5 cursor-pointer">
