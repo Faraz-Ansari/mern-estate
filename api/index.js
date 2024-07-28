34146ade9a013c45016d94714dd145ea40ff830c
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-07-29 01:55:10 +0530

    add pre production changes

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ mongoose
 const app = express();
 
 const port = 3000;
+const __dirname = path.resolve();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -32,6 +34,14 @@ app.listen(port, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+// Serve static assets if in production 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+// serve the index.html file if in production
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+}); 
 
 // Error handling middleware
 app.use((err, req, res, next) => {
