c93a836a4b6c533d755b89c9e1b1477c54279209
Ahmad Faraz Ansari <Frzansari20@gmail.com>
2024-07-28 17:25:19 +0530

    rename getListings api to search api to avoid confusion

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -59,7 +59,7 @@ export const getListing = async (req, res, next) => {
     }
 };
 
-export const getListings = async (req, res, next) => {
+export const search = async (req, res, next) => {
     try {
         const limit = parseInt(req.query.limit) || 9;
         const startIndex = parseInt(req.query.startIndex) || 0;
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -5,7 +5,7 @@ import {
     deleteListing,
     updateListing,
     getListing,
-    getListings,
+    search,
 } from "../controllers/listing.controller.js";
 
 const router = express.Router();
@@ -14,6 +14,6 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/get/:id", getListing);
-router.get("/get", getListings);
+router.get("/get", search);
 
 export default router;
